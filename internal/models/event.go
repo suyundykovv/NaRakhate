de25d92d420368c8bee83293ed4a0405f4d205de
suyundykovv <margulansuyundykov@gmail.com>
2025-01-30 10:59:14 +0600

    structured logic of project, create deployment, frontend, backend

--- internal/models/event.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Event struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	StartTime   string `json:"start_time"`
-	Category    string `json:"category"`
-}
